3cf0c684947a432f709332d07b2a5ae8e55fb450
bochkov <bochkov-vi@yandex.ru>
2025-06-05 10:12:01 +0300

    fix: корректная работа при SSR

--- src/composables/use.ol.ts
+++ src/composables/use.ol.ts
@@ -13,19 +13,23 @@ export function useOlMap(
   mount?: (map: OlMap) => void,
   unmount?: (map: OlMap) => void
 ) {
-  const olMap = inject('olMap', undefined) as undefined | Ref<OlMap | undefined>
-  onMounted(() => {
-    const map = toValue(olMap)
-    if (map && mount) {
-      mount(map)
+  const olMap = inject<Ref<OlMap | undefined>>('olMap')
+
+  function onMapChange(nv?: OlMap, ov?: OlMap) {
+    if (nv && mount) {
+      mount(nv)
     }
+    if (ov && unmount) {
+      unmount(ov)
+    }
+  }
+
+  onMounted(() => {
+    onMapChange(toValue(olMap))
   })
   onUnmounted(() => {
-    const map = toValue(olMap)
-    if (map && unmount) {
-      unmount(map)
-    }
+    onMapChange(undefined, toValue(olMap))
   })
-
+  watch(() => toValue(olMap), onMapChange)
   return { olMap }
 }
